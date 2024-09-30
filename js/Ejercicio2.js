7c49a3f46821b1cb91c4229f6c2bfa7df6fe4198
pilarsd99 <pilarsd99@gmail.com>
2024-09-30 10:08:10 -0300

    Ejercicio2

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -6,4 +6,11 @@ const mascotas = [
 'popi',
 'catrina',
 ];
-console.log(mascotas[3])
+console.log(mascotas[3]);
+// Asignacion de valores
+console.log(mascotas[5]);
+mascotas[5] = 'Michi nuevo';
+console.log(mascotas[5]);
+// agregar elemento
+mascotas.push('Almendro',);
+console.log(mascotas);

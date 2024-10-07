73f21f1373c59367ce8ae02da33e55f309101e61
pilarsd99 <pilarsd99@gmail.com>
2024-10-07 10:01:59 -0300

    Ejercicio3POO

--- js/Ejercicio3.js
+++ js/Ejercicio3.js
@@ -14,6 +14,15 @@ const Persona2 = {
         return 'Hola ' + this.nombre;
     }
 }
+function crearPersona(nombre, edad){
+    const obj = {};
+    obj.nombre = nombre;
+    obj.edad = edad;
+    obj.saludar = function(){
+        return 'Hola soy ' + this.nombre;
+    }
+    return obj;
+}
 console.log(Persona.nombre);
 console.log(Persona.edad);
 Persona.edad = 21;
@@ -21,3 +30,5 @@ console.log('nueva edad: '+ Persona.edad);
 console.log('Con []: '+ Persona['nombre']);
 console.log(Persona.saludar());
 console.log(Persona2.saludar());
+const Tita = crearPersona('Tita', 30);
+console.log(Tita);
